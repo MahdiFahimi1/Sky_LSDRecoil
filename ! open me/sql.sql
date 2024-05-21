2eefbb5a51da17685d79486cd33206f2bde5ce91
MahdiFahimi1 <m.mahdi.lind@gmail.com>
2024-05-21 09:39:23 -0700

    Script Files Uploaded
    Script FIles

---
+++ ! open me/sql.sql
@@ -0,0 +1,5 @@
+USE `es_extended`; -- your database
+
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+	('lsd', 'LSD', 5)
+;
---
+++ config.lua
@@ -0,0 +1,33 @@
+Config = {}
+
+
+------------------------------------Base-------------------------------------
+
+Config.Base = 'newesx' -- 'oldesx' or 'newesx'
+
+------------------------------------Base-------------------------------------
+
+
+------------------------------------Notif------------------------------------
+
+Config.Notify = 'Sky_Notification' -- Your Notify
+
+------------------------------------Notif------------------------------------
+
+
+-----------------------------------System------------------------------------
+
+Config.SystemMessage = "Sky_LSDRecoil" -- System Message
+
+-----------------------------------System------------------------------------
+
+
+-------------------------------------LSD-------------------------------------
+
+Config.Use1 = true
+Config.Time = 600 -- Use Time
+Config.Timetext = "LSD Enabled For 10 Min" -- LSD Time (Text)
+Config.BTime = "LSD Is Enable For You" -- When LSD Is Enable And You Use It Agin
+Config.EndLSD = "LSD Disabled" -- Text For Disable LSD
+
+-------------------------------------LSD-------------------------------------
